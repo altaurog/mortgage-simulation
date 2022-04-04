db15bf2b8c198ec845244a2ed2cde0e54d4757ba
Aryeh Leib Taurog <git@aryehleib.com>
2022-04-05 01:41:38 +0300

    mwe point reset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-import InterestRates from './InterestRates';
+import * as d3 from 'd3';
 
-render(<InterestRates years={30}/>, document.getElementById('root'));
+const owidth = 800;
+const oheight = 300;
+
+const container = document.createElement('div');
+document.body.append(container);
+const root = ReactDOM.createRoot(container);
+root.render(<Drag/>);
+
+function Drag() {
+  const svgRef = React.useRef(null);
+  const [points, setPoints] = React.useState([
+    {id: 0, x: 20, y: 50},
+    {id: 1, x: 700, y: 250},
+  ]);
+
+  function dragDrag(event, d) {
+    const point = {
+      ...d,
+      x: event.x,
+      y: event.y,
+    };
+    const newPoints = points.map(p => p.id === point.id ? point : p);
+    setPoints(newPoints);
+  }
+
+  function update(el) {
+    return el.select("g")
+      .selectAll("circle")
+      .data(points, d => d.id)
+      .join("circle")
+      .attr("r", 5)
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+  }
+
+  React.useEffect(() => {
+    const el = d3.select(svgRef.current);
+    update(el);
+  }, [points]);
+
+  React.useEffect(() => {
+    const el = d3.select(svgRef.current);
+    update(el)
+      .call(d3.drag()
+      .on("drag", dragDrag));
+  }, []);
+
+  return (
+    <svg
+      ref={svgRef}
+      height={oheight}
+      width={owidth}
+      viewBox={`0 0 ${owidth} ${oheight}`}
+    >
+      <g/>
+    </svg>
+  );
+};
