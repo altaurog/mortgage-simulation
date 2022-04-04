90ce37ac433e21cd18cae83605117077195306f1
Aryeh Leib Taurog <git@aryehleib.com>
2022-04-05 01:11:39 +0300

    initial commit (sort of working)

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import InterestRates from './InterestRates';
+
+render(<InterestRates years={30}/>, document.getElementById('root'));
