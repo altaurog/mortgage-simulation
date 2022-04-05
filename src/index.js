9e83d854ddc81b17a4eaf270ff36414a82d4b540
Aryeh Leib Taurog <git@aryehleib.com>
2022-04-05 17:56:27 +0300

    refactor: split d3 from react

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore } from 'redux';
-import { Provider, useSelector, useDispatch } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 
 import * as d3 from 'd3';
 
-const owidth = 800;
-const oheight = 300;
+// redux
 const initialState = [
   {id: 0, x: 20, y: 50},
   {id: 1, x: 700, y: 250},
 ];
-const store = createStore(points, initialState);
 
-const container = document.createElement('div');
-document.body.append(container);
-const root = ReactDOM.createRoot(container);
-root.render(
-  <Provider store={store}>
-    <Drag/>
-  </Provider>
-);
+const store = createStore(points, initialState);
 
 function points(state, action) {
   switch (action.type) {
@@ -32,50 +23,63 @@ function points(state, action) {
   }
 }
 
-function Drag() {
+
+// react
+const container = document.createElement('div');
+document.body.append(container)
+const root = ReactDOM.createRoot(container);
+root.render(
+  <Provider store={store}>
+    <Data/>
+  </Provider>
+);
+
+function Data() {
   const points = useSelector(state => state);
-  const dispatch = useDispatch();
-  const svgRef = React.useRef(null);
-
-  function dragDrag(event, d) {
-    const point = {
-      ...d,
-      x: event.x,
-      y: event.y,
-    };
-    dispatch({type: 'point/drag', payload: point});
-  }
-
-  function update(el) {
-    return el.select("g")
-      .selectAll("circle")
-      .data(points, d => d.id)
-      .join("circle")
-      .attr("r", 5)
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y);
-  }
-
-  React.useEffect(() => {
-    const el = d3.select(svgRef.current);
-    update(el);
-  }, [points]);
-
-  React.useEffect(() => {
-    const el = d3.select(svgRef.current);
-    update(el)
-      .call(d3.drag()
-      .on("drag", dragDrag));
-  }, []);
 
   return (
-    <svg
-      ref={svgRef}
-      height={oheight}
-      width={owidth}
-      viewBox={`0 0 ${owidth} ${oheight}`}
-    >
-      <g/>
-    </svg>
+    <div>
+      {points.map(p => <p key={p.id}>({p.x}, {p.y})</p>)}
+    </div>
   );
 };
+
+
+// d3
+const owidth = 800;
+const oheight = 300;
+
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', owidth)
+  .attr('height', oheight)
+  .attr('viewBox', `0 0 ${owidth} ${oheight}`);
+
+function render() {
+  const state = store.getState();
+  return update(svg, state);
+}
+
+store.subscribe(render);
+render()
+  .call(d3.drag()
+  .on("drag", dragDrag));
+
+function update(svg, points) {
+  console.log(points);
+  return svg.selectAll("circle")
+    .data(points, d => d.id)
+    .join("circle")
+    .attr("r", 5)
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y);
+}
+
+function dragDrag(event, d) {
+  const point = {
+    ...d,
+    x: event.x,
+    y: event.y,
+  };
+  store.dispatch({type: 'point/drag', payload: point});
+}
